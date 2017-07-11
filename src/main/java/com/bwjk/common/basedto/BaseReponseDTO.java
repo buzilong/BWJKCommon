922021e4febca31a7ad34a0ee1c08f22b4b75d04
buzilong <bzlwork@126.com>
2017-07-12 00:22:48 +0800

    更新项目结构

---
+++ src/main/java/com/bwjk/common/basedto/BaseReponseDTO.java
@@ -0,0 +1,24 @@
+package com.bwjk.common.basedto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseReponseDTO {
+
+	private List<ErrorInfo> errorList;
+
+	public List<ErrorInfo> getErrorList() {
+		return errorList;
+	}
+
+	public void setErrorList(List<ErrorInfo> errorList) {
+		this.errorList = errorList;
+	}
+
+	public void addError(ErrorInfo errorInfo) {
+		if (errorList == null) {
+			errorList = new ArrayList<>();
+		}
+		errorList.add(errorInfo);
+	}
+}
---
+++ src/main/java/com/bwjk/common/basedto/ErrorInfo.java
@@ -0,0 +1,33 @@
+package com.bwjk.common.basedto;
+
+public class ErrorInfo {
+
+	private String message;
+	private String code;
+	private String type;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
